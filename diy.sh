59c36a1e98165b240fb18b89cc9b8631d3930295
starblink <starblink@localhost.com>
2020-03-22 23:03:34 +0800

    modify diy.sh add custom[1-3]

--- diy.sh
+++ diy.sh
@@ -7,5 +7,45 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168.1.1/192.168.1.251/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.251/g'           package/base-files/files/bin/config_generate
 sed -i '1a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
+sed -i '/query_method=/s/tcp_only/udp_tcp/'      package/lean/luci-app-ssr-plus/root/etc/init.d/shadowsocksr
+custom1=`cat <<-'EOF'
+#modify\\
+#127.0.0.1:7055:1081@1.0.0.1:853\\
+echo "$dnsstr" |grep '@' && custom_socks5port=$(echo "$dnsstr" | awk -F '@' '{print $1}' |awk -F ':' '{print $3}') || custom_socks5port=$(echo "$dnsstr" |awk -F ':' '{print $3}')\\
+custom_dns=$(echo "$dnsstr" | awk -F '@' '{print $2}')
+EOF
+`
+
+custom2=`cat <<-'EOF'
+#modify\\
+custom_socks5port=${custom_socks5port:=1081}\\
+custom_dns=${custom_dns:=8.8.4.4}\\
+netstat -tupln |grep -q $custom_socks5port && (dns2socks 127.0.0.1:$custom_socks5port $custom_dns 127.0.0.1:7055 >/root/dns.log 2>&1 &) || echo "Nomatch this socks5 port!" >/root/dns.log
+EOF
+`
+
+custom3=`cat <<-'EOF'
+#modify\\
+# killall -q -9 trojan\\
+ps -ef |grep trojan-ssr-retcp |grep -v grep |awk '$0=$1' |xargs kill -9\\
+kill -9 $(busybox ps -w | grep trojan-ssr-socksdns | grep -v grep | awk '{print $1}') >/dev/null 2>&1\\
+kill -9 $(busybox ps -w | grep trojan-ssr-netflix | grep -v grep | awk '{print $1}') >/dev/null 2>&1
+EOF
+`
+
+
+sed -i -e '/local dnsport/{a '"$custom1"' ' -e'}' package/lean/luci-app-ssr-plus/root/etc/init.d/shadowsocksr
+
+
+sed -i -e '/start() /{
+:a
+/{/{x;s/^/./;x}
+/}/{x;s/.//;/./!{x;i '"$custom2"' ' -e b}';x}
+n
+ba
+}' package/lean/luci-app-ssr-plus/root/etc/init.d/shadowsocksr
+
+
+sed -i 's%killall.*trojan%'"$custom3"'%' package/lean/luci-app-ssr-plus/root/etc/init.d/shadowsocksr
